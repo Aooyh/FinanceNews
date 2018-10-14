eec311ecf9162122352081212ab40fbb77d65a06
Aooyh <aoo_yh@163.com>
2018-10-14 21:19:54 +0800

    icon 和 验证码 请求实现

--- info/modules/index/__init__.py
+++ info/modules/index/__init__.py
@@ -1,8 +1,10 @@
+from flask import current_app
 from flask import render_template
 from flask import Blueprint, session
+from info.utils.captcha.captcha import captcha
 import info
 
-index_blue = Blueprint('index', __name__, static_folder='../../static', template_folder='../../templates/news')
+index_blue = Blueprint('index', __name__, static_folder='../../static/news', template_folder='../../templates/news')
 
 
 @index_blue.route('/')
@@ -11,3 +13,14 @@ def index():
     session['name'] = 'yh'
     redis_store.set('gender', 'male')
     return render_template('index.html')
+
+
+@index_blue.route('/favicon.ico')
+def send_ico():
+    return current_app.send_static_file('favicon.ico')
+
+
+@index_blue.route('/image_code')
+def image_code():
+    image_data = captcha.generate_captcha()[2]
+    return image_data
