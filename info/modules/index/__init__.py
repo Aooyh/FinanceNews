69139267a26907df0d77ac6151684f3a85591a70
Aooyh <aoo_yh@163.com>
2018-10-14 20:30:22 +0800

    数据库表迁移

--- info/modules/index/__init__.py
+++ info/modules/index/__init__.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask import Blueprint, session
 import info
 
-index_blue = Blueprint('index', __name__, static_folder='../static', template_folder='../templates/news')
+index_blue = Blueprint('index', __name__, static_folder='../../static', template_folder='../../templates/news')
 
 
 @index_blue.route('/')
@@ -10,4 +10,4 @@ def index():
     redis_store = info.create_app()[2]
     session['name'] = 'yh'
     redis_store.set('gender', 'male')
-    return render_template()
+    return render_template('index.html')
--- manager.py
+++ manager.py
@@ -1,10 +1,10 @@
-from info import create_app
 from flask_script import Manager
 from info.modules.index import index_blue
 from flask_migrate import MigrateCommand, Migrate
+from info.models import *
 
 
-app, db, redis_store = create_app('testing')
+app = create_app('testing')[0]
 app.register_blueprint(index_blue)
 manager = Manager(app)
 Migrate(app, db)
